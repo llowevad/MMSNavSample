0c3912be1ff86267de785ef75cbb89109f6b8f7d
llowevad <dave.wollerman@outlook.com>
2015-04-06 12:35:39 -0400

    Initial Commit

---
+++ MetadataNavigation/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="0e851c83-e6f4-47ae-b247-4f7306cab08c" description="SharePoint App Feature" featureId="0e851c83-e6f4-47ae-b247-4f7306cab08c" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="MetadataNavigation Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f869eb95-9712-4c72-9ba5-05f55dcaf0f4" />
+    <projectItemReference itemId="3be3c589-5d4e-4a24-9f0c-f2343698eb70" />
+    <projectItemReference itemId="3fd6dd42-8a9e-4391-899a-7e60c196e2aa" />
+    <projectItemReference itemId="4e19cb67-e41c-475d-bec4-8700888590cf" />
+  </projectItems>
+</feature>
---
+++ MetadataNavigation/Scripts/Terms.js
@@ -0,0 +1,98 @@
+﻿var context = SP.ClientContext.get_current();
+var termsHTML;
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+$(document).ready(function () {
+    getTermSet('Taxonomy_2wzqpytxkbENkepeLOcgqg==', '8135b4c9-7d06-4d50-8bc7-67e3137f4d95');
+});
+
+function getTermSet(termStoreName, termSetId) {
+
+    //Current Taxonomy Session
+    var taxSession = SP.Taxonomy.TaxonomySession.getTaxonomySession(context);
+
+    //Term Stores
+    var termStores = taxSession.get_termStores();
+
+    //Name of the Term Store from which to get the Terms.
+    var termStore = termStores.getByName(termStoreName);
+
+    //GUID of Term Set from which to get the Terms.
+    var termSet = termStore.getTermSet(termSetId);
+    var terms = termSet.get_terms();
+
+    context.load(terms);
+    context.executeQueryAsync(function () {
+
+        var termEnumerator = terms.getEnumerator();
+        termsHTML = "<ul id=\"navmenu\">";
+
+        while (termEnumerator.moveNext()) {
+            var currentTerm = termEnumerator.get_current();
+            var termId = currentTerm.get_id();
+            var termName = currentTerm.get_name();
+            var childTermCount = currentTerm.get_termsCount();
+            var props = currentTerm.get_localCustomProperties();
+
+            var navLink = props["_Sys_Nav_SimpleLinkUrl"];
+
+            termsHTML += "<li id=\"" + termId + "\"><a href=\"" + navLink + "\">" + termName + " (" + childTermCount + ")</a></li>";
+
+            //Check if term has child terms.
+            if (childTermCount > 0) {
+
+                //Term has sub terms.
+                recursiveTerms(currentTerm);
+            }
+        }
+
+        termsHTML += "</ul>";
+        $('#TopMenu').html(termsHTML);
+
+    }, onGetFail);
+}
+
+function recursiveTerms(currentTerm) {
+
+    //Get Term child terms
+    var terms = currentTerm.get_terms();
+
+    context.load(terms);
+    context.executeQueryAsync(function () {
+        getTerms(terms, currentTerm);
+    }, onGetFail);
+
+}
+
+function getTerms(terms, parent) {
+    var termsEnum = terms.getEnumerator();
+    var childTermsHTML = "<ul>";
+    while (termsEnum.moveNext()) {
+
+        var newCurrentTerm = termsEnum.get_current();
+        var termId = newCurrentTerm.get_id();
+        var termName = newCurrentTerm.get_name();
+        var childTermCount = newCurrentTerm.get_termsCount();
+        var props = newCurrentTerm.get_localCustomProperties();
+
+        var navLink = props["_Sys_Nav_SimpleLinkUrl"];
+
+        childTermsHTML += "<li id=\"" + termId + "\"><a href=\"" + navLink + "\">" + termName + " (" + childTermCount + ")</a></li>";
+
+        //Check if term has child terms.
+        if (childTermCount > 0) {
+
+            //Term has sub terms.
+            recursiveTerms(newCurrentTerm);
+        }
+    }
+
+    var HTMLID = "#" + parent.get_id();
+    $(HTMLID).append(childTermsHTML + "</ul>");
+    childTermsHTML = "";
+}
+
+// This function is executed if the call fails
+function onGetFail(sender, args) {
+    alert('Error:' + args.get_message());
+}
